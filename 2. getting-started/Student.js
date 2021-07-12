58e054e79a7f2d2dd1da9c28adb1c0a0fa6d2d41
been <pjb6510@naver.com>
2021-07-13 04:47:01 +0900

    Add getting-started

---
+++ 2. getting-started/Student.js
@@ -0,0 +1,8 @@
+var Student = /** @class */ (function () {
+    function Student(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    return Student;
+}());
+var studentA = new Student("A", 23);
