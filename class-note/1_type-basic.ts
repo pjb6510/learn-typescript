febcc42eae057ea3a26884475205a2e335ae7825
been <pjb6510@naver.com>
2021-07-14 18:49:01 +0900

    Add type-basic.ts

---
+++ class-note/1_type-basic.ts
@@ -0,0 +1,27 @@
+// 문자
+const str: string = "hello world";
+
+// 숫자
+const num: number = 123;
+
+// 배열
+const arr: Array<number> = [1, 2, 3];
+const heroes : Array<string> = ["Capt", "Thor", "Hulk"];
+const items: number[] = [1, 2, 3];
+
+// 튜플: 모든 인덱스의 타입이 정의되어 있는 배열.
+const address: [string, number] = ["gangnam", 10];
+
+// 객체
+const obj: object = {};
+const person1: object = {
+  name: "Jason",
+  age: 28,
+};
+const person2: { name: string, age: number } = {
+  name: "Eli",
+  age: 3,
+};
+
+// 불린
+let isLoading: boolean = true;
